7dc6a8d1148f872c1ed4344e5eaaa51fc5119511
irfanramadhan11 <o.irfanramadhan.o@gmail.com>
2025-01-04 04:53:56 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,24 @@ document.querySelector('#menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
 // Klik di luar sidebar untuk menghilangkan nav
+const srcbtn = document.querySelector('#search-button');
 const menu = document.querySelector('#menu');
 
 document.addEventListener('click', function (e) {
+    if (!srcbtn.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
     if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
